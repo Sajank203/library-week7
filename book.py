aa2769df8fd9cc7577e1a80dfc29afddf7e32f60
Sajank203 <SAJANPREET1712003@ICLOUD.COM>
2024-09-05 13:20:58 +1200

    Add files via upload
    book week 7

---
+++ book.py
@@ -0,0 +1,18 @@
+#Book.py
+class book:
+    def __init__(self, title, author, is_borrowed=False):
+       self.title = title
+       self.author = author
+       self.is_borrowed = is_borrowed
+    def borrow(self):  
+        if not self.is_borrowed:
+            self.is_borrowed = True
+            return True
+        else:
+            return False
+    def return_book(self):
+        if self.is_borrowed:
+            self.is_borrowed = False
+            return True
+        else:
+            return false    
